90c355c390d5e7c1e47590208d522af54f359711
ramajan-tahashildar <ramajantahashildar07@gmail.com>
2024-11-09 11:28:17 +0530

    Committed
    Committed

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st
 st.title("GitHub-Workshop")
 st.header("GIT, Belagavi")
+
+name= st.text_input(label="Student Name", placeholder="Enter student name")
+collageName= st.text_input(label="Collage", placeholder="Enter collage name")
+email= st.text_input(label="Email", placeholder="Enter student email")
+
+
+if st.button("Submit"):
+    if name=="" or collageName=="" or email=="" :
+        st.error("Please fill all the fields")
+    else:
+        st.success("Form submitted successfully")
+
+
